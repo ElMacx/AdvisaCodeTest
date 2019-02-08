8f9508c2568b5709e439db850cf86c6c845292b0
Maxime DULIN <maximedulin@gmail.com>
2019-02-08 17:28:59 +0100

    Move app content to fileContent
    Signed-off-by: Maxime DULIN <maximedulin@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import { InputTile } from "./Components/InputTile/InputTile";
-import { ResultTile } from "./Components/ResultTile/ResultTile";
+import { TileGroup } from "./Views/TileGroup/TileGroup";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      values: ["0", "0", "0"],
-    };
-  }
-
-  handleInputChange = event => {
-    const tmpArray = this.state.values;
-    tmpArray[event.currentTarget.id] = event.currentTarget.value;
-    this.setState({ values: tmpArray})
-  };
-
   render() {
-    const { values } = this.state;
-    return (
-      <div className="app">
-        <h1>Advisa code test</h1>
-        {values.map((v, index) => {
-          return (
-            <InputTile
-              key={index}
-              id={index}
-              label={`Value ${index + 1} :`}
-              value={v}
-              handleInputChange={this.handleInputChange}
-            />);
-        })}
-        <ResultTile values={values}/>
-      </div>
-    );
+    return <TileGroup />;
   }
 }
 
